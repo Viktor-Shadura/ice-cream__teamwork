74d23b5af187f28e41984bd901c47da095370ff5
Viktor-Shadura <110778522+Viktor-Shadura@users.noreply.github.com>
2022-10-16 18:43:50 +0000

    Deploying to gh-pages from @ Viktor-Shadura/ice-cream__teamwork@e7c0e079fc4fcf121072a74eab218fcf5042ccfa 🚀

--- index.1e191f06.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-products-open]"),closeModalBtn:document.querySelector("[data-modal-products-close]"),modal:document.querySelector("[data-modal-products]")};function e(){o.modal.classList.toggle("is-products-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.1e191f06.js.map
---
+++ index.592f6af4.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtns:document.querySelectorAll("[data-modal-products-open]"),closeModalBtn:document.querySelector("[data-modal-products-close]"),modal:document.querySelector("[data-modal-products]")};function e(){o.modal.classList.toggle("is-products-hidden")}o.openModalBtns.forEach((function(o){return o.addEventListener("click",e)})),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.592f6af4.js.map
