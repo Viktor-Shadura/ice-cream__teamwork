d01807f3c4bf36e099c28f2a5936c7d97eb90e1b
Viktor-Shadura <110778522+Viktor-Shadura@users.noreply.github.com>
2022-10-17 19:29:21 +0000

    Deploying to gh-pages from @ Viktor-Shadura/ice-cream__teamwork@4afdd4eba536e341a216adcc2e53d47c0f897fd4 🚀

---
+++ index.db8ab330.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtns:document.querySelectorAll("[data-modal-header-open]"),closeModalBtn:document.querySelector("[data-modal-header-close]"),modal:document.querySelector("[data-modal-header]")};e.openModalBtns.forEach((function(e){return e.addEventListener("click",toggleModalByNow)})),e.closeModalBtn.addEventListener("click",toggleModalByNow)}();
+//# sourceMappingURL=index.db8ab330.js.map
